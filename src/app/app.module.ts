7e0fc9dd169d8828f6506afb6fe3a7afb581c4dd
christophercr <christophercr@gmail.com>
2020-09-11 10:56:03 +0200

    integrate Store Devtools and ngrx-store-logger to track state actions and changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {EffectsModule} from '@ngrx/effects';
 
 import {AppComponent} from './app.component';
 import {BookModule} from './book/book.module';
 
 import {SharedModule} from './shared/shared.module';
-import { reducers, metaReducers } from './store';
-import { AppEffects } from './effects/app.effects';
+import {metaReducers, reducers} from './store';
+import {AppEffects} from './effects/app.effects';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { AppEffects } from './effects/app.effects';
     BookModule,
     SharedModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot(reducers, {metaReducers}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,9 +1,17 @@
-import {ActionReducerMap, MetaReducer} from '@ngrx/store';
+import {ActionReducer, ActionReducerMap, MetaReducer} from '@ngrx/store';
+import {storeLogger} from "ngrx-store-logger";
 import {environment} from '../../environments/environment';
 
 export interface State {
   // empty root state
 }
+
 export const reducers: ActionReducerMap<State> = {};
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+// console.log all actions
+export function logger(reducer: ActionReducer<State>): any {
+  // default, no options
+  return storeLogger()(reducer);
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger] : [];
